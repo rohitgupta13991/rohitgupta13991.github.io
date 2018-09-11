c29f9f673c722ef7ca226063ad9c3fd1f1cdabd4
Rohit Gupta <rohit.gupta@wingify.com>
2018-09-11 18:09:39 +0530

    changed account files

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/b07b11d313752b7366fb223a8d24c9fb.js');
+importScripts('https://cdn.pushcrew.com/sw/22cb197dc17d90eec5047b724be7221b.js');
