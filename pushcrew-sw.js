a4076a0a4d8fe32ce94e33024d187f1554899dc1
rohitgupta13991 <rohit.gupta@wingify.com>
2018-09-17 14:10:45 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrewstaging.com/sw/b9f6176d4fd32e8ed6d64fc13c292f61.js');
+importScripts('https://cdn.pushcrew.com/sw/626f5b6298261804de8890a93ac7418e.js');
